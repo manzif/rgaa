3722d99bb272973f80d272ba1f458c4a3cff4cee
Manzi Fabrice <manzif60@gmail.com>
2020-10-08 17:01:55 +0200

    add advocacy

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,6 +111,21 @@
     });
   });
 
+
+  // Intro carousel
+  var heroCarousel = $("#heroCarousel");
+  var heroCarouselIndicators = $("#hero-carousel-indicators");
+  heroCarousel.find(".carousel-inner").children(".carousel-item").each(function (index) {
+    (index === 0) ?
+      heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>") :
+      heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
+  });
+
+  heroCarousel.on('slid.bs.carousel', function (e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
+  });
+
   // Back to top button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
@@ -132,6 +147,7 @@
     autoplay: true,
     dots: true,
     loop: true,
+    easing: 'easeinout',
     responsive: {
       0: {
         items: 1
@@ -172,6 +188,9 @@
   $(".portfolio-details-carousel").owlCarousel({
     autoplay: true,
     dots: true,
+    speed: 1000,
+    timeout: 0,
+    easing: 'easeinout',
     loop: true,
     items: 1
   });
